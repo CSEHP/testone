d04e91a33d5acdd0e95c6d975b92b8e0ff3fda95
W&N <WAN13693893877@outlook.com>
2020-12-31 12:57:46 +0800

    php 新增 图片添加水印

---
+++ waterMaker/index.php
@@ -0,0 +1,8 @@
+<?php
+//定义图片的位置
+include ('waterMark.php');
+header('content-type:text/html;charset=utf-8');
+$imagePath = './image.jpg';
+$waterMarkPath ='./watermark.png';
+
+makeWaterMark($imagePath,$waterMarkPath,'rightDown');
---
+++ waterMaker/waterMark.php
@@ -0,0 +1,58 @@
+<?php
+
+function makeWaterMark($imagePath, $waterMarkPath,$place='rightDown')
+{
+    $fromFunction = [
+        'image/jpeg' => 'imagecreatefromjpeg',
+        'image/png' => 'imagecreatefrompng',
+        'image/gif' => 'imagecreatefromgif'
+    ];
+
+    $toFunction = [
+        'image/jpeg' => 'imagejpeg',
+        'image/png' => 'imagepng',
+        'image/gif' => 'imagegif',
+    ];
+
+//获取图片信息
+    $imageInfo = getimagesize($imagePath);
+    list($imageWidth, $imageHeight) = $imageInfo;
+    $imageMime = $imageInfo['mime'];
+
+    $waterInfo = getimagesize($waterMarkPath);
+    list($waterWidth, $waterHeight) = $waterInfo;
+    $waterMine = $waterInfo['mime'];
+
+// 根据图片创建画布
+    $image = $fromFunction[$imageMime]($imagePath);
+    $waterImage = $fromFunction[$waterMine]($waterMarkPath);
+
+//给目标图片加水印
+    $srcX = 0;
+    $srcY = 0;
+    if($place=='rightDown'){
+        $srcX = $imageWidth - $waterWidth;
+        $srcY = $imageHeight - $waterHeight;
+    }else if($place=='rightUp'){
+        $srcX = $imageWidth - $waterWidth;
+        $srcY = 0;
+    }else if($place=='leftUp'){
+        $srcX =0;
+        $srcY =0;
+    }else if($place=='leftDown'){
+        $srcX = 0;
+        $srcY = $imageHeight - $waterHeight;
+    }else{
+        exit('您输入的水印位置不存在！！');
+    }
+
+
+    imagecopy($image, $waterImage, $srcX, $srcY, 0, 0, $waterWidth, $waterHeight);
+
+    header("content-type:$imageMime");
+    $toFunction[$imageMime]($image);
+
+//销毁画布
+    imagedestroy($image);
+    imagedestroy($waterImage);
+}
